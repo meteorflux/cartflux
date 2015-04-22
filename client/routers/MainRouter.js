a236463de94988f5a6092a3b7344cd417bf6fb8d
Luis Herranz <luisherranz@gmail.com>
2015-04-22 18:45:12 +0200

    refactor router

--- client/routers/MainRouter.js
+++ client/routers/MainRouter.js
@@ -13,6 +13,22 @@ var MainRouter = function () {
     }
   };
 
+  FlowRouter.route(routes.home, {
+    action: function() {
+      Dispatcher.dispatch({ actionType: "GO_TO_HOME" });
+    }
+  });
+  FlowRouter.route(routes.catalog, {
+    action: function(params) {
+      Dispatcher.dispatch({ actionType: "GO_TO_CATALOG_PAGE", page: 1 });
+    }
+  });
+  FlowRouter.route(routes.catalog + ':page', {
+    action: function(params) {
+      Dispatcher.dispatch({ actionType: "GO_TO_CATALOG_PAGE", page: params.page });
+    }
+  });
+
   return self;
 
 };
